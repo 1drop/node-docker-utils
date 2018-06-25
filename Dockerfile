0774bde9d5816d29393e4552755329b44774dc7e
Markus Sommer <msommer@1drop.de>
2018-06-25 21:13:42 +0200

    [TASK] Add shopware linting

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM node:8-slim
 
 RUN npm install --production --loglevel warn -g eslint eslint-config-standard eslint-plugin-promise eslint-plugin-standard eslint-plugin-import@latest eslint-plugin-node@latest > /dev/null
+
+COPY ./scripts /home/node
+
+RUN chmod +x ./*.sh
+
 WORKDIR /home/node
 
 CMD ["/bin/bash"]
---
+++ scripts/eslint-shopware.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+$SHOPWARE_FOLDER = 'app/shopware'
+
+cd $SHOPWARE_FOLDER
+
+# CUSTOM PLUGINS
+if [[ -n $(find custom/plugins/Od*/**/ -name '*.js') ]]; then
+    echo "linting js files in custom plugins";
+    find custom/plugins/Od*/**/ -name '*.js' -print0 | xargs -0 eslint -c .eslintrc.js --quiet
+    # catch exit code
+    if [ ! $? -eq 0 ]; then
+      exit 1
+    fi
+fi
+# LOCAL PLUGINS
+if [[ -n $(find engine/Shopware/Plugins/Local/*/Od*/**/ -name '*.js') ]]; then
+    echo "linting js files in local plugins";
+    find engine/Shopware/Plugins/Local/*/Od*/**/ -name '*.js' -print0 | xargs -0 eslint -c .eslintrc.js --quiet
+    # catch exit code
+    if [ ! $? -eq 0 ]; then
+      exit 1
+    fi
+fi
+# THEMES
+if [[ -n $(find themes/Frontend/**/ -name '*.js') ]]; then
+    echo "linting js files in themes";
+    find themes/Frontend/**/ -name '*.js' -print0 | xargs -0 eslint -c .eslintrc.js --quiet
+    # catch exit code
+    if [ ! $? -eq 0 ]; then
+      exit 1
+    fi
+fi
