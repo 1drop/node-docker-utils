376fe9251919ad3a63d16e8ada51fd02a86c57fc
Markus Sommer <msommer@1drop.de>
2018-07-16 13:32:29 +0200

    [FEATURE] Add Plugin eslint

---
+++ bin/eslint-shopware-plugin.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /home/node/
+
+if [[ -n $(find ./**/ -name '*.js') ]]; then
+    echo "linting js files";
+    find ./**/ -name '*.js' -print0 | xargs -0 eslint -c .eslintrc.js --quiet
+    # catch exit code
+    if [ ! $? -eq 0 ]; then
+      exit 1
+    fi
+fi
